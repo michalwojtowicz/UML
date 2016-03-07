34feaad2145cd2b41d6f25170900ffbbe5484a0c
wojtowicz.michal <wojtowicz.michal@outlook.com>
2016-03-07 17:21:07 +0100

    test

---
+++ test/test/test.cpp
@@ -0,0 +1,15 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	cout << "test";
+
+    return 0;
+}
+
