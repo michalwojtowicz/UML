733839bae2173c35ddf5e608d51ec2e11fce7a21
pindlu94 <pindlu94@gmail.com>
2016-05-26 21:13:34 +0200

    Screen do testów

---
+++ test/test/Screan.cpp
@@ -0,0 +1,65 @@
+
+#include "stdafx.h"
+#include "Screan.h"
+
+#include <iostream>
+
+Screan::Screan(){
+	for (int i = 0; i < 15; ++i) {
+
+		for (int j = 0; j < 15; ++j) {
+			tab[i][j] = false;
+			tabInt[i][j] = 0;
+		}
+	}
+}
+
+
+Screan::~Screan(){
+}
+
+void Screan::print() {
+	std::cout << "mapa z moimi statkami" << std::endl;
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j <10; ++j) {
+			std::cout << tab[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+}
+
+void Screan::printInt() {
+	std::cout << "mapa do ztrza³u" << std::endl;
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j <10; ++j) {
+			std::cout << tabInt[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+}
+
+void Screan::clean() {
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			tab[i][j] = false;
+		}
+	}
+}
+
+void Screan::read(const Sea *sea) {
+	int x;
+	int y;
+	PartShip *p;
+	int I = sea->getI();
+	for (int i = 0; i < I; i++) {
+		p = sea->tab[i];
+		x = p->getX();
+		y = p->getY();
+		tab[y][x] = p->getStan();
+	}
+
+}
+
+void Screan::setField(int x, int y, int stan) {
+	tabInt[y][x] = stan;
+}
---
+++ test/test/Screan.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Sea.h"
+
+class Screan
+{
+public:
+	Screan();
+	~Screan();
+
+	void setField(int x, int y, int stan);
+	void print();
+	void printInt();
+	void clean();
+	void read(const Sea *sea);
+
+private:
+	bool tab[10][10];
+	int tabInt[10][10];
+
+};
+
